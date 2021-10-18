7a0525429c9fd5108bef2dd0166df319a263f864
hicham2121 <91796172+hicham2121@users.noreply.github.com>
2021-10-19 00:06:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
 function sleep(ms) {
+  
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const docker_username = process.env.DOCKER_USERNAME;
+  console.log(`DOCKER_USERNAME = ${docker_username}`);
   while(true) {
     console.log("Microservices rock!");
     await sleep(5000);
